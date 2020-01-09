68b8dbe099c34d041172bdaeba254ac0f4f10d45
chgibb <chris.gibb@outlook.com>
2020-01-09 23:32:21 +0000

    fixed linter

--- lib/fluatter.dart
+++ lib/fluatter.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Fluatter {
-  static const MethodChannel _channel =
-      const MethodChannel('fluatter');
+  static const MethodChannel _channel = const MethodChannel('fluatter');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
